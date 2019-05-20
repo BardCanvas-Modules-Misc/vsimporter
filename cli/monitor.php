6a64db3be6d5f76819a26d1e4d6abf18e21d6c0f
Alejandro Caballero <lava.caballero@gmail.com>
2019-05-20 07:37:05 -0500

    Added support for multiple source feeds

--- cli/monitor.php
+++ cli/monitor.php
@@ -39,30 +39,41 @@ if( ! is_writable($config->datafiles_location) )
     die();
 }
 
-$feed_url = $settings->get("modules:vsimporter.feed_url");
-
-if( empty($feed_url) )
+$raw_sources = $settings->get("modules:vsimporter.feeds");
+if( empty($raw_sources) )
 {
     cli_colortags::write(
-        "<red>[$now] Critical: feed URL unset</red>\n" .
-        "<red>Please open the settings editor and define the VS feed URL.</red>\n" .
+        "<red>[$now] Critical: source feeds unset</red>\n" .
+        "<red>Please open the settings editor and define the VS feeds to fetch.</red>\n" .
         "<red>Aborting.</red>\n\n"
     );
     
     die();
 }
 
-$item_url_prefix = $settings->get("modules:vsimporter.item_url_prefix");
-
-if( empty($item_url_prefix) )
+$feed_sources = array();
+foreach(explode("\n", $raw_sources) as $line)
 {
-    cli_colortags::write(
-        "<red>[$now] Critical: item URL prefix unset</red>\n" .
-        "<red>Please open the settings editor and define the VS item URL prefix.</red>\n" .
-        "<red>Aborting.</red>\n\n"
-    );
+    $line  = trim($line);
+    $parts = explode("\t", $line);
+    $name  = trim($parts[0]);
     
-    die();
+    $feed_sources[$name] = (object) array(
+        "url"      => trim($parts[1]),
+        "prefix"   => trim($parts[2]),
+        "last_pid" => "",
+    );
+}
+
+$raw_pids = $settings->get("modules:vsimporter.last_article_id");
+if( ! empty($raw_pids) )
+{
+    foreach(explode("\n", $raw_pids) as $line)
+    {
+        $line = trim($line);
+        $name = trim(current(explode(":", $line)));
+        if( isset($feed_sources[$name]) ) $feed_sources[$name]->last_pid = trim(end(explode(":", $line)));
+    }
 }
 
 $raw_categories = $settings->get("modules:vsimporter.categories");
@@ -198,115 +209,22 @@ $featured_keywords     = empty($raw_featured_keywords) ? array() : preg_split('/
 if( empty($featured_tag) )      cli_colortags::write("<brown> * Note: featured tag is empty. No posts will be set as featured.</brown> ");
 if( empty($featured_keywords) ) cli_colortags::write("<brown> * Note: featured keywords list is empty. No posts will be set as featured.</brown> ");
 
-$offset = $settings->get("modules:vsimporter.last_article_id");
-if( $offset == "" )
+foreach( $feed_sources as $feed_name => &$feed_data )
 {
-    cli_colortags::write("<light_gray> > Fetching feed...</light_gray> ");
-}
-else
-{
-    $feed_url .= "&o=$offset";
-    cli_colortags::write("<white> > Fetching feed starting from offset $offset...</white> ");
-}
-
-$ch = curl_init();
-curl_setopt( $ch, CURLOPT_URL,            $feed_url);
-curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true     );
-curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, 5        );
-curl_setopt( $ch, CURLOPT_TIMEOUT,        10       );
-curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true     );
-$t   = time();
-$res = curl_exec($ch);
-if( curl_error($ch) )
-{
-    $error = curl_error($ch);
-    
-    cli_colortags::write(
-        "\n<yellow>Error fetching the feed:</yellow>\n" .
-        "<yellow>$error</yellow>\n" .
-        "<yellow>Aborting.</yellow>\n\n"
-    );
-    @unlink(LOCK_FILE);
-    
-    die();
-}
-cli_colortags::write(sprintf(
-    "<light_green>OK!</light_green> <white>%s KiB downloaded in %s seconds.</white>\n",
-    number_format(strlen($res) / 1024, 2),
-    time() - $t
-));
-curl_close($ch);
-
-$feed = json_decode($res);
-if( empty($feed) )
-{
-    cli_colortags::write(
-        "<yellow>Feed malformed!</yellow>\n" .
-        "<yellow>The feed contents doesn't seem to be JSON encoded.</yellow>\n" .
-        "<yellow>Please check the URL and fetch it manually, then validate it.</yellow>\n" .
-        "<yellow>Aborting.</yellow>\n\n"
-    );
-    @unlink(LOCK_FILE);
-    
-    die();
-}
-
-if( empty($feed->pages) )
-{
-    cli_colortags::write("<light_purple> > Feed seems to be empty.</light_purple>\n");
-    cli_colortags::write("<cyan>Feed integration finished.</cyan>\n");
-    $seconds = time() - $start;
-    cli_colortags::write("<cyan>Finished in $seconds seconds.</cyan>\n\n");
-    @unlink(LOCK_FILE);
-    
-    die();
-}
-
-$feed->pages = (array) $feed->pages;
-
-$lastpid = 0;
-$index   = 0;
-$count   = count($feed->pages);
-cli_colortags::write("<light_gray> ┌ Starting loop on $count pages...</light_gray>\n");
-foreach($feed->pages as $page)
-{
-    if( $page->id > $lastpid ) $lastpid = $page->id;
-    $index++;
-    
-    # Pre-forging
-    $post = new post_record(array(
-        "id_post"         => "420" . sprintf("%09.0f", $page->id),
-        "slug"            => wp_sanitize_filename($page->title),
-        "title"           => ucwords($page->title),
-        "excerpt"         => make_excerpt_of($page->microdata->summary, $excerpt_length),
-        "visibility"      => "public",
-        "status"          => "published",
-        "allow_comments"  => "1",
-        "creation_date"   => date("Y-m-d H:i:s", strtotime($page->microdata->date)),
-        "publishing_date" => date("Y-m-d H:i:s", strtotime($page->microdata->date)),
-        "last_udpate"     => date("Y-m-d H:i:s"),
-    ));
-    
-    # Check for existence by permalink
-    $src_key = "vsimporter.source_url";
-    $plink   = addslashes(serialize($page->url));
-    $filter  = array("id_post in (select post_meta.id_post from post_meta where post_meta.name = '$src_key' and post_meta.value = '$plink')");
-    $found   = $posts_repository->get_record_count($filter);
-    if( $found > 0 )
+    $feed_url = $feed_data->url;
+    $offset   = $feed_data->last_pid;
+    if( $offset == "" )
     {
-        # cli_colortags::write("<yellow> │ ($index/$count) Post #$post->id_post '$post->title' exists - skipped.</yellow>\n");
-        
-        continue;
+        cli_colortags::write("<light_gray> > Fetching feed </light_gray><light_blue>$feed_name</light_blue><light_gray>...</light_gray> ");
+    }
+    else
+    {
+        $feed_url .= "&o=$offset";
+        cli_colortags::write("<white> > Fetching feed </white><light_blue>$feed_name</light_blue><white> starting from offset $offset...</white> ");
     }
     
-    # Notify
-    cli_colortags::write("<green> │ > ($index/$count) Got #$post->id_post '$post->title'.</green>\n");
-    
-    # Fetch article
-    $article_url = $item_url_prefix . urlencode($page->url);
-    cli_colortags::write("<light_gray> │   Fetching article...</light_gray> ");
     $ch = curl_init();
-    curl_setopt( $ch, CURLOPT_URL,            $article_url);
+    curl_setopt( $ch, CURLOPT_URL,            $feed_url);
     curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true     );
     curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, 5        );
     curl_setopt( $ch, CURLOPT_TIMEOUT,        10       );
@@ -316,14 +234,15 @@ foreach($feed->pages as $page)
     if( curl_error($ch) )
     {
         $error = curl_error($ch);
-        cli_colortags::write(
-            "\n" .
-            "<yellow> │   Error fetching the article!</yellow>\n" .
-            "<yellow> │   $error</yellow>\n" .
-            "<yellow> │   Article skipped.</yellow>\n"
-        );
         
-        continue;
+        cli_colortags::write(
+            "\n<yellow>Error fetching the feed:</yellow>\n" .
+            "<yellow>$error</yellow>\n" .
+            "<yellow>Aborting.</yellow>\n\n"
+        );
+        @unlink(LOCK_FILE);
+        
+        die();
     }
     cli_colortags::write(sprintf(
         "<light_green>OK!</light_green> <white>%s KiB downloaded in %s seconds.</white>\n",
@@ -331,91 +250,195 @@ foreach($feed->pages as $page)
         time() - $t
     ));
     curl_close($ch);
-    $obj = json_decode($res);
-    if( empty($obj) )
+    
+    $feed = json_decode($res);
+    if( empty($feed) )
     {
         cli_colortags::write(
-            "<yellow> │   Warning: article malformed! It seems to be an invalid JSON object. Skipping it</yellow>\n"
+            "<yellow>Feed malformed!</yellow>\n" .
+            "<yellow>The feed contents doesn't seem to be JSON encoded.</yellow>\n" .
+            "<yellow>Please check the URL and fetch it manually, then validate it.</yellow>\n" .
+            "<yellow>Aborting.</yellow>\n\n"
         );
+        @unlink(LOCK_FILE);
         
-        continue;
+        die();
     }
-    if( empty($obj->pages) )
+    
+    if( empty($feed->pages) )
     {
-        cli_colortags::write(
-            "<yellow> │   Warning: article doesn't seem to have contents. Skipping it.</yellow>\n"
-        );
+        cli_colortags::write("<light_purple> > Feed seems to be empty.</light_purple>\n");
+        cli_colortags::write("<cyan>Feed integration finished.</cyan>\n");
+        $seconds = time() - $start;
+        cli_colortags::write("<cyan>Finished in $seconds seconds.</cyan>\n\n");
+        @unlink(LOCK_FILE);
         
-        continue;
+        die();
     }
-    $article     = current($obj->pages);
-    /** @noinspection HtmlDeprecatedTag */
-    $contents    = strip_tags($article->summary, "<b><i><em><strong><u><strike><sup><sub><br>");
-    $paragraphs  = explode("\n\n", $contents);
-    $raw_content = "$post->title\n\n";
-    foreach($paragraphs as $paragraph)
+    
+    $feed->pages = (array) $feed->pages;
+    
+    $lastpid = 0;
+    $index   = 0;
+    $count   = count($feed->pages);
+    cli_colortags::write("<light_gray> ┌ Starting loop on $count pages...</light_gray>\n");
+    foreach($feed->pages as $page)
     {
-        $paragraph = trim($paragraph);
+        if( $page->id > $lastpid ) $lastpid = $page->id;
+        $index++;
         
-        if( empty($paragraph) ) continue;
-        if( $paragraph == "." ) continue;
+        # Pre-forging
+        $post = new post_record(array(
+            "id_post"         => "420" . sprintf("%09.0f", $page->id),
+            "slug"            => wp_sanitize_filename($page->title),
+            "title"           => ucwords($page->title),
+            "excerpt"         => make_excerpt_of($page->microdata->summary, $excerpt_length),
+            "visibility"      => "public",
+            "status"          => "published",
+            "allow_comments"  => "1",
+            "creation_date"   => date("Y-m-d H:i:s", strtotime($page->microdata->date)),
+            "publishing_date" => date("Y-m-d H:i:s", strtotime($page->microdata->date)),
+            "last_udpate"     => date("Y-m-d H:i:s"),
+        ));
         
-        $paragraph      = preg_replace('/ \./', "", $paragraph);
-        $raw_content   .= "$paragraph\n\n";
-        $post->content .= "<p>$paragraph</p>\n";
-    }
-    $raw_content = strtolower($raw_content);
-    
-    # Scan categories and tags
-    $tags = array();
-    set_category_and_tags();
-    if( ! empty($tags) ) $post->content .= "<p>#" . implode(" #", $tags) . "</p>";
-    
-    # Reforge slug if necessary
-    $tmp = $posts_repository->get_record_count(array("slug like '{$post->slug}%'"));
-    if( $tmp > 0 ) $post->slug .= $tmp;
-    
-    # Forge author
-    forge_author();
-    
-    # Featured image
-    if( ! empty($page->main_image_url) )
-    {
-        $item = get_featured_image($page->main_image_url, $page->url);
-        if( ! empty($item) )
+        # Check for existence by permalink
+        $src_key = "vsimporter.source_url";
+        $plink   = addslashes(serialize($page->url));
+        $filter  = array("id_post in (select post_meta.id_post from post_meta where post_meta.name = '$src_key' and post_meta.value = '$plink')");
+        $found   = $posts_repository->get_record_count($filter);
+        if( $found > 0 )
         {
-            $post->id_featured_image = $item->id_media;
+            # cli_colortags::write("<yellow> │ ($index/$count) Post #$post->id_post '$post->title' exists - skipped.</yellow>\n");
             
-            $post->content = sprintf(
-                    '<p class="aligncenter"><img src="%s" data-id-media="%s" data-media-type="image"></p>' . "\n",
-                    $item->get_item_url(),
-                    $item->id_media
-                ) . $post->content;
+            continue;
         }
+        
+        # Notify
+        cli_colortags::write("<green> │ > ($index/$count) Got #$post->id_post '$post->title'.</green>\n");
+        
+        # Fetch article
+        $article_url = $feed_data->prefix . urlencode($page->url);
+        cli_colortags::write("<light_gray> │   Fetching article...</light_gray> ");
+        $ch = curl_init();
+        curl_setopt( $ch, CURLOPT_URL,            $article_url);
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true     );
+        curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, 5        );
+        curl_setopt( $ch, CURLOPT_TIMEOUT,        10       );
+        curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true     );
+        $t   = time();
+        $res = curl_exec($ch);
+        if( curl_error($ch) )
+        {
+            $error = curl_error($ch);
+            cli_colortags::write(
+                "\n" .
+                "<yellow> │   Error fetching the article!</yellow>\n" .
+                "<yellow> │   $error</yellow>\n" .
+                "<yellow> │   Article skipped.</yellow>\n"
+            );
+            
+            continue;
+        }
+        cli_colortags::write(sprintf(
+            "<light_green>OK!</light_green> <white>%s KiB downloaded in %s seconds.</white>\n",
+            number_format(strlen($res) / 1024, 2),
+            time() - $t
+        ));
+        curl_close($ch);
+        $obj = json_decode($res);
+        if( empty($obj) )
+        {
+            cli_colortags::write(
+                "<yellow> │   Warning: article malformed! It seems to be an invalid JSON object. Skipping it</yellow>\n"
+            );
+            
+            continue;
+        }
+        if( empty($obj->pages) )
+        {
+            cli_colortags::write(
+                "<yellow> │   Warning: article doesn't seem to have contents. Skipping it.</yellow>\n"
+            );
+            
+            continue;
+        }
+        $article     = current($obj->pages);
+        /** @noinspection HtmlDeprecatedTag */
+        $contents    = strip_tags($article->summary, "<b><i><em><strong><u><strike><sup><sub><br>");
+        $paragraphs  = explode("\n\n", $contents);
+        $raw_content = "$post->title\n\n";
+        foreach($paragraphs as $paragraph)
+        {
+            $paragraph = trim($paragraph);
+            
+            if( empty($paragraph) ) continue;
+            if( $paragraph == "." ) continue;
+            
+            $paragraph      = preg_replace('/ \./', "", $paragraph);
+            $raw_content   .= "$paragraph\n\n";
+            $post->content .= "<p>$paragraph</p>\n";
+        }
+        $raw_content = strtolower($raw_content);
+        
+        # Scan categories and tags
+        $tags = array();
+        set_category_and_tags();
+        if( ! empty($tags) ) $post->content .= "<p>#" . implode(" #", $tags) . "</p>";
+        
+        # Reforge slug if necessary
+        $tmp = $posts_repository->get_record_count(array("slug like '{$post->slug}%'"));
+        if( $tmp > 0 ) $post->slug .= $tmp;
+        
+        # Forge author
+        forge_author();
+        
+        # Featured image
+        if( ! empty($page->main_image_url) )
+        {
+            $item = get_featured_image($page->main_image_url, $page->url);
+            if( ! empty($item) )
+            {
+                $post->id_featured_image = $item->id_media;
+                
+                $post->content = sprintf(
+                        '<p class="aligncenter"><img src="%s" data-id-media="%s" data-media-type="image"></p>' . "\n",
+                        $item->get_item_url(),
+                        $item->id_media
+                    ) . $post->content;
+            }
+        }
+        
+        $posts_repository->save($post);
+        $post->set_meta($src_key, $page->url);
+        
+        if( ! empty($post->id_featured_image) ) $posts_repository->set_media_items(array($post->id_featured_image), $post->id_post);
+        
+        cli_colortags::write("<green> │   Post saved.</green>");
+        
+        if( ! empty($tags) )
+        {
+            sort($tags);
+            $posts_repository->set_tags($tags, $post->id_post);
+            cli_colortags::write("<light_green> Tags: #" . implode(" #", $tags) . "</light_green>");
+        }
+        
+        cli_colortags::write("\n");
     }
+    cli_colortags::write("<light_gray> └ Loop finished.</light_gray>\n");
     
-    $posts_repository->save($post);
-    $post->set_meta($src_key, $page->url);
-    
-    if( ! empty($post->id_featured_image) ) $posts_repository->set_media_items(array($post->id_featured_image), $post->id_post);
-    
-    cli_colortags::write("<green> │   Post saved.</green>");
-    
-    if( ! empty($tags) )
+    if( ! empty($lastpid) )
     {
-        sort($tags);
-        $posts_repository->set_tags($tags, $post->id_post);
-        cli_colortags::write("<light_green> Tags: #" . implode(" #", $tags) . "</light_green>");
+        $feed_data->last_pid = $last_pid;
+        cli_colortags::write("<light_cyan> > Last article id set to $lastpid</light_cyan>\n");
     }
     
-    cli_colortags::write("\n");
+    cli_colortags::write("<light_cyan> > Feed $feed_name finished.</light_cyan>\n");
 }
-cli_colortags::write("<light_gray> └ Loop finished.</light_gray>\n");
-if( ! empty($lastpid) )
-{
-    $settings->set("modules:vsimporter.last_article_id", $lastpid);
-    cli_colortags::write("<light_cyan>Last article id set to $lastpid</light_cyan>\n");
-}
+
+$lastpids_value = "";
+foreach($feed_sources as $feed_name => $feed_data)
+    $lastpids_value .= "{$feed_name}: {$feed_data->last_pid}\n";
+$settings->set("modules:vsimporter.last_article_id", $lastpids_value);
 
 cli_colortags::write("<cyan>Feed integration finished.</cyan>\n");
 
